0a421ac4f4acaee39178527aee27eb730d6ac921
Giuseppe Masino <dev.giuseppemasino@gmail.com>
2019-07-13 09:30:29 +0200

    Configure nlohmann/json library include

--- projectFiles/makeModules/libSpec.mk
+++ projectFiles/makeModules/libSpec.mk
@@ -9,10 +9,25 @@ $(v_lib_obj)    := $(lib_objdir)/libpistache.a
 
 $(v_lib_dflags) :=
 $(v_lib_lflags) := -lpthread
-$(v_lib_iflags) := -Ilib/pistache/include
+$(v_lib_iflags) := -I$(lib_dir)/include
 
 $(lib.pistache.objdir)/libpistache.a:
 	mkdir -p $(lib.pistache.dir)/build
 	cd $(lib.pistache.dir)/build ; cmake .. ; make
+
+-include $(libEnable)
+
+# LIB SECTION: Json for Modern C++
+# NOTE: HEADER-ONLY LIBRARY
+
+libname := nlohmann-json
+
+$(v_lib_dir)    := lib/json
+$(v_lib_objdir) :=
+$(v_lib_obj)    :=
+
+$(v_lib_dflags) :=
+$(v_lib_lflags) :=
+$(v_lib_iflags) := -I$(lib_dir)/single_include
 
 -include $(libEnable)
