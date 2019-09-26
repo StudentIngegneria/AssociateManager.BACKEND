f4560befaa3b5f97b732ed8cd76fed585d7ed869
Giuseppe Masino <dev.giuseppemasino@gmail.com>
2019-09-27 00:48:51 +0200

    improvement(lib): Switch from vanilla sqlite3 to an OO c++ wrapper lib

--- projectFiles/makeModules/libSpec.mk
+++ projectFiles/makeModules/libSpec.mk
@@ -33,19 +33,21 @@ $(v_lib_ldflags)  :=
 
 $(v_lib_clean):
 # Nothing to clean
-
+#
 -include $(libEnable)
 
-# LIB SECTION: sqlite3
+# LIB SECTION: sqlite_modern_cpp
 
-libname := sqlite3
+libname := sqlite_modern_cpp
 
-$(v_lib_dir)      :=
+$(v_lib_dir)      := lib/$(libname)
 $(v_lib_objdir)   :=
 $(v_lib_obj)      :=
 
-$(v_lib_cppflags) :=
-$(v_lib_ldflags)  := $(shell $(PKG_CONFIG) --libs sqlite3)
+$(v_lib_cppflags) := -I$(lib_dir)/hdr
+$(v_lib_ldflags)  := -lsqlite3
 
 $(v_lib_clean):
 # Nothing to clean
+
+include $(libEnable)
