788bfc08bbc6d7ba964ca5f42e115f2c49b16f6f
Giuseppe Masino <dev.giuseppemasino@gmail.com>
2019-10-14 17:29:50 +0200

    improvement(lib): Remove Pistache Using cpp-httplib instead

--- projectFiles/makeModules/libSpec.mk
+++ projectFiles/makeModules/libSpec.mk
@@ -1,24 +1,5 @@
 # External libraries makefile
 
-# LIB SECTION: Pistache
-libname := pistache
-
-$(v_lib_dir)      := lib/pistache
-$(v_lib_objdir)   := $(lib_dir)/build/src
-$(v_lib_obj)      := $(lib_objdir)/libpistache.a
-
-$(v_lib_cppflags) := -I$(lib_dir)/include
-$(v_lib_ldflags)  := -lpthread
-
-$(lib_objdir)/libpistache.a:
-	mkdir -p $(lib.pistache.dir)/build
-	cd $(lib.pistache.dir)/build ; cmake .. ; make
-
-$(v_lib_clean):
-	cd $(lib.pistache.dir)/build ; make clean
-
-include $(libEnable)
-
 # LIB SECTION: cpp-httplib
 
 libname := cpp-httplib
