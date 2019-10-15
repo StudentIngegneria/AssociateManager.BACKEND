c717ae260d2acca0a7f27c2ca8d4d9d3a23c5657
Giuseppe Masino <dev.giuseppemasino@gmail.com>
2019-10-15 15:47:50 +0200

    feature(cli): Add cli handling code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
-int main() {
-		
+#include <iostream>
+#include <cstdlib>
+
+#include <Flags.hh>
+
+int main( const int argc, char ** argv ) {
+
+		std::string serverAddress = "localhost" ;
+		uint64_t serverPort = 1234 ;
+
+		{
+			bool displayHelp = false ;
+
+			Flags cliParser ;
+			cliParser.Var( serverAddress, char(), "ip-interface", std::string( "localhost" ) , "The ip address from which listen" ) ;
+			cliParser.Var( serverPort, char(), "port", uint64_t(1234), "The network port" ) ;
+			cliParser.Bool( displayHelp, 'h', "help", "Display the quick help" ) ;
+
+			if( ! cliParser.Parse( argc, argv ) || displayHelp ) {
+					cliParser.PrintHelp( argv[0] ) ;
+					std::exit( EXIT_SUCCESS ) ;
+				}
+		}
 	}
